ef177b2e3e23e1e6b68cf415df0c4112906eb13d
atiya-inayat <atiyainayat869@gmail.com>
2025-12-29 12:46:14 +0500

    completing backend for imagekit

--- app/api/imagekit-auth/route.ts
+++ app/api/imagekit-auth/route.ts
@@ -2,14 +2,21 @@
 import { getUploadAuthParams } from "@imagekit/next/server"
 
 export async function GET() {
-   
-    const authenticationParameters = getUploadAuthParams({
-        privateKey: process.env.IMAGEKIT_PRIVATE_KEY as string, // Never expose this on client side
-        publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY as string,
-      
-    })
-
+try {
+       
+        const authenticationParameters = getUploadAuthParams({
+            privateKey: process.env.IMAGEKIT_PRIVATE_KEY as string, // Never expose this on client side
+            publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY as string,
+          
+        })
+    
+        return Response.json({ 
+            authenticationParameters, 
+            publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY })
+    }
+catch (error) {
     return Response.json({ 
-        authenticationParameters, 
-        publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY })
+           error: "Authentication for Imagekit failed"
+    }, {status: 500})
 }
+} 
