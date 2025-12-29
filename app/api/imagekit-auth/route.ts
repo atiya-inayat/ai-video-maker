815ed7f3ec03b8ad046f8e617c5effc1a3a571d9
atiya-inayat <atiyainayat869@gmail.com>
2025-12-29 11:42:34 +0500

    setup for imagekit

---
+++ app/api/imagekit-auth/route.ts
@@ -0,0 +1,15 @@
+// File: app/api/upload-auth/route.ts
+import { getUploadAuthParams } from "@imagekit/next/server"
+
+export async function GET() {
+   
+    const authenticationParameters = getUploadAuthParams({
+        privateKey: process.env.IMAGEKIT_PRIVATE_KEY as string, // Never expose this on client side
+        publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY as string,
+      
+    })
+
+    return Response.json({ 
+        authenticationParameters, 
+        publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY })
+}
